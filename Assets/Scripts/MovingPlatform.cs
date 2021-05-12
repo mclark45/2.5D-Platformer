4e90cfb93650d2f801eaf045623ae66c84a34d42
mclark45 <80595429+mclark45@users.noreply.github.com>
2021-05-12 18:54:56 -0400

    added rigidbody to the moving platform and cleaned up scripts

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -8,19 +8,9 @@ public class MovingPlatform : MonoBehaviour
     private Transform _pointA, _pointB;
     [SerializeField]
     private float _speed = 1.0f;
-    [SerializeField]
-    private float _amplitude = 3.5f;
-    [SerializeField]
-    private float _frequency = 2f;
-    private float _xOffSet = 30f;
     private bool _changedirection = false;
 
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         MovePlatforms();
@@ -45,19 +35,12 @@ public class MovingPlatform : MonoBehaviour
         {
             _changedirection = true;
         }
-
-        /*float x = Mathf.Cos(Time.time) * _amplitude * _frequency;
-        float y = transform.position.y;
-        float z = transform.position.z;
-
-        transform.position = new Vector3(x + _xOffSet, y, z);*/
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player is marked as Player on Enter");
             other.transform.SetParent(this.transform);
         }
     }
@@ -66,7 +49,6 @@ public class MovingPlatform : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player is marked as Player on Exit");
             other.transform.SetParent(null);
         }
     }
