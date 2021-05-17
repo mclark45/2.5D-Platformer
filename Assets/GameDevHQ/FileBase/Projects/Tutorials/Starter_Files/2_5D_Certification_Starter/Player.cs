edf7b7a63b298dce6d2be115ea6bea0db96cb92c
mclark45 <80595429+mclark45@users.noreply.github.com>
2021-05-17 19:50:56 -0400

    adding player movement

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5.0f;
+    [SerializeField] private float _gravity = 1.0f;
+    [SerializeField] private float _jumpHeight = 5.0f;
+    private float _playersYAxisVelocity;
+    private CharacterController _controller;
+
+    void Start()
+    {
+        _controller = gameObject.GetComponent<CharacterController>();
+
+        if (_controller == null)
+        {
+            Debug.LogError("Character Controller is Null");
+        }
+    }
+
+    
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+
+        Vector3 playerMovement = new Vector3(0, 0, horizontalMovement);
+        Vector3 playerVelocity = playerMovement * _speed;
+
+        if (_controller.isGrounded == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                
+                playerVelocity.y = _jumpHeight;
+            }
+        }
+        else
+        {
+            playerVelocity.y -= _gravity;
+        }
+
+        _controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
