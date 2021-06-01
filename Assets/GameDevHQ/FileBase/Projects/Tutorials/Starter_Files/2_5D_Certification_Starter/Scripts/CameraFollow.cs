c8cf762e5709524fd76412af9769089165a8228e
mclark45 <80595429+mclark45@users.noreply.github.com>
2021-06-01 17:23:38 -0400

    adding animations

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    private Vector3 _cameraPositon;
+
+    private void Start()
+    {
+        _cameraPositon = transform.position - _player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = _player.position + _cameraPositon;
+    }
+}
--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
@@ -7,16 +7,24 @@ public class Player : MonoBehaviour
     [SerializeField] private float _speed = 5.0f;
     [SerializeField] private float _gravity = 1.0f;
     [SerializeField] private float _jumpHeight = 5.0f;
+    private bool _jumping = false;
     private float _playersYAxisVelocity;
     private CharacterController _controller;
+    private Animator _playerAnim;
 
     void Start()
     {
         _controller = gameObject.GetComponent<CharacterController>();
+        _playerAnim = GetComponentInChildren<Animator>();
 
         if (_controller == null)
         {
             Debug.LogError("Character Controller is Null");
+        }
+
+        if (_playerAnim == null)
+        {
+            Debug.LogError("Player Animator controller is Null");
         }
     }
 
@@ -28,17 +36,32 @@ public class Player : MonoBehaviour
 
     private void PlayerMovement()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal");
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
 
         Vector3 playerMovement = new Vector3(0, 0, horizontalMovement);
         Vector3 playerVelocity = playerMovement * _speed;
+        _playerAnim.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+
+       if (horizontalMovement != 0)
+        {
+            Vector3 playerDirection = transform.localEulerAngles;
+            playerDirection.y = playerMovement.z > 0 ? 0 : 180;
+            transform.localEulerAngles = playerDirection;
+        }
 
         if (_controller.isGrounded == true)
         {
+            if (_jumping == true)
+            {
+                _jumping = false;
+                _playerAnim.SetBool("Jump", _jumping);
+            }
+
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                
                 _playersYAxisVelocity = _jumpHeight;
+                _jumping = true;
+                _playerAnim.SetBool("Jump", _jumping);
             }
         }
         else
